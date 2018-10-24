4e7447144c820563e45790a5ce2385f5f22048de
Matt25969 <matt.jo.hunt@gmail.com>
2018-10-24 09:18:02 +0100

    added report

--- src/test/java/com/qa/ExampleCIProject/LogicTest.java
+++ src/test/java/com/qa/ExampleCIProject/LogicTest.java
@@ -4,10 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class LogicTest {
+	
+	public ExtentReports report;
+	public ExtentTest test;
 	
 	@Test
 	public void playTest() {
+		
+		report = new ExtentReports(".\\BasicReport.html", true);
+
+		test = report.startTest("Verify application Title");
+		
+		test.log(LogStatus.INFO, "Browser started");
+		
+		test.log(LogStatus.PASS, "verify Title of the page");
+		
+		report.endTest(test);
+		report.flush();
 		
 		assertEquals(10,Logic.play(10,10));
 		
